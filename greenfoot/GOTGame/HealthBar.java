0b1080ac4dcbb7ea1e587f933abd5f474887bff3
Sai Prithipa Ram Mohan <sainishved@Sais-MacBook-Air.local>
2019-04-20 18:29:17 -0700

    User Story 2 - Task 1 and Task 2 submission

---
+++ greenfoot/GOTGame/HealthBar.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HealthBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HealthBar extends Actor
+{
+    /**
+     * Act - do whatever the HealthBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public HealthBar() {
+        GreenfootImage bg = new GreenfootImage("health.png");
+        bg.scale(300, 60);
+        setImage(bg);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
