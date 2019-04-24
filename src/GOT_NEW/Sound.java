139fbb0d477a2b9df18bc3479f5b894525b39912
Karthika Arumugam <karthika.arumugam@sjsu.edu>
2019-04-23 23:58:48 -0700

    Start Screen

---
+++ src/GOT_NEW/Sound.java
@@ -0,0 +1,74 @@
+import greenfoot.*; 
+/**
+ * Write a description of class Sound here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sound  
+{
+    // instance variables - replace the example below with your own
+    private GreenfootSound gameOverSound;
+    private GreenfootSound explosionSound;
+    private GreenfootSound powerUpSound;
+    private GreenfootSound backgroundMusic;
+    private GreenfootSound gamemusic;
+    private static Sound single_instance = null; 
+    public static Sound getInstance() 
+    { 
+        if (single_instance == null) 
+            single_instance = new Sound(); 
+  
+        return single_instance; 
+    } 
+    /**
+     * Constructor for objects of class Sound
+     */
+    private Sound()
+    {
+        try {
+            //gameOverSound = new GreenfootSound("GameOver.wav");
+            //explosionSound = new GreenfootSound("explosion.wav");
+            gamemusic = new GreenfootSound("gamemusic.mp3");
+            backgroundMusic = new GreenfootSound("background.mp3");
+            
+        } catch(Exception e){}
+    }
+
+
+    public void playGameOver() {
+        try {
+            gameOverSound.play();
+        } catch(Exception e){}
+    }
+
+    public void playBackGroundMusic() {
+        try {
+             // backgroundMusic.playLoop();      
+        } catch(Exception e){}
+    }
+
+    public void playPowerUp() {
+        try {
+            powerUpSound.play();
+        } catch(Exception e){}
+    }
+    
+   public void playgamemusic() {
+        try {
+            gamemusic.playLoop();
+        } catch(Exception e){}
+    }
+
+    public void playExplosion() {
+        try {
+            explosionSound.play();
+        } catch(Exception e){}
+    }
+
+    public void pauseBackGround() {
+        try {
+            backgroundMusic.pause();
+        } catch(Exception e){}
+    }
+}
