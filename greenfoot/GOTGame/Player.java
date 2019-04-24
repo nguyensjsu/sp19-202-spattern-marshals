c721203c9bdc1e0c4f83bbd385cd2c777f6062e3
Sai Prithipa Ram Mohan <sainishved@Sais-MacBook-Air.local>
2019-04-23 23:49:02 -0700

    Implementation of observer pattern in various actors

--- greenfoot/GOTGame/Player.java
+++ greenfoot/GOTGame/Player.java
@@ -7,29 +7,39 @@ import java.util.ArrayList;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Hero extends Actor
+public class Player extends Actor
 {
     GifImage gifw = new GifImage("walk.gif");
     GifImage gifa = new GifImage("attack.gif");
     GreenfootImage gifi = new GreenfootImage("Idle.png");
+    HealthBar healthBar;
+    private List<PlayerObserver> observers = new ArrayList<PlayerObserver>();
+    
+    public Player() {
+        
+    }
     /**
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        
+        addObservers();
         gifi.scale(100,135);
         setImage(gifi);
         if (Greenfoot.isKeyDown("left")) {
             gifw.resizeImages(300,200);
             setImage(gifw.getCurrentImage());
             setLocation(getX() - 2, getY());
+            MyWorld world = (MyWorld) getWorld();
+            world.scroll();
         }
         else if (Greenfoot.isKeyDown("right")) {
             gifw.resizeImages(300,200);
             setImage(gifw.getCurrentImage());
             setLocation(getX() + 2, getY());
+            MyWorld world = (MyWorld) getWorld();
+            world.scroll();
         }
         else if (Greenfoot.isKeyDown("space")) {
             gifa.resizeImages(300,200);
@@ -45,10 +55,26 @@ public class Hero extends Actor
         for (Opponent o : opponents) {
             int dist = getX() - o.getX();
             if (Math.abs(dist) < 5) {
-                getWorld().removeObject(o);
-                GreenfootImage img = getWorld().getObjects(HealthBar.class).get(0).getImage();
-                img.scale(img.getWidth() - 50, img.getHeight());
+                notifyObservers("hit");
             }
+        }
+    }
+    
+    private void addObservers() {
+        healthBar = getWorld().getObjects(HealthBar.class).get(0);
+        if (!observers.contains(healthBar))
+            observers.add(healthBar);
+        List<Opponent> opponents = getWorld().getObjects(Opponent.class);
+        for (Opponent opponent : opponents) {
+            if (!observers.contains(opponent)) {
+                observers.add(opponent);
+            }
+        }
+    }
+    
+    private void notifyObservers(String type) {
+        for(PlayerObserver observer : observers) {
+            observer.playerUpdate(type);
         }
     }
 }
