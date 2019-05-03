f1e5d8c757e8ce5f57e6280e34c25c69860a50dd
anushreemenon <47263849+anushreemenon@users.noreply.github.com>
2019-05-03 12:55:19 -0700

    Update GameOver.java

--- ScoreBoard Component/GameOver.java
+++ ScoreBoard Component/GameOver.java
@@ -1,11 +1,40 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 
-public class GameOver extends Actor
+public class GameOver extends Actor implements IGameOverObserver
 {
     
     public GameOver() 
     {
-        setImage(new GreenfootImage("Game Over", 48, Color.WHITE, Color.BLACK));
+        //setImage(new GreenfootImage("Game Over", 48, Color.WHITE, Color.BLACK));
     }    
+   
+    public void act() 
+    {
+       
+      
+}
+  public void gameOverUpdate(MyWorld w)
+  {
+      
+        System.out.println("Observer added");
+        GreenfootImage image = this.getImage();
+
+       
+        Font font = image.getFont();
+        font = font.deriveFont(FONT_SIZE);
+        image.setFont(font);
+        image.setColor(Color.WHITE);
+        image.drawString("Game Over", 150, 300);
+        //image.drawString(prefix + scoreValue, 150, 300);
+        setImage(image);
+       
+         w.addObject (this, w.getWidth()/2,w.getHeight()/2);
+       // music.playGameOver();
+       Sound.getInstance().playGameOver();
+        // End program
+        Greenfoot.stop();  
+    }
+
+
 }
